78e4faf175849de24333c56a1eb63abd0138861c
sam <subterranean.layer@gmail.com>
2024-05-18 15:34:43 -0400

    add home route, latest products view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,20 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
+
 export default function App() {
-	return <h1>Welcome to ProShop</h1>;
+	return (
+		<>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1>Welcome To ProShop</h1>
+					<Outlet />
+				</Container>
+			</main>
+			<Footer />
+		</>
+	);
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home';
+
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<App />}>
+			<Route index={true} path='/' element={<Home />} />
+		</Route>
+	)
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router}></RouterProvider>
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
